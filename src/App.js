8ee4f78c2b8639e5dd9e549bc72a0ad4e628d9f0
Sumit <sumitsati12770.ss@gmail.com>
2024-07-12 14:57:03 +0530

    new

--- src/App.js
+++ src/App.js
@@ -6,19 +6,48 @@ import Navbar from "./component/Navbar.js";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <GateSessionCard
-        name="Ayush Adhikari"
-        handle="@Aerospace Engineer"
-        degree="M.Tech"
-        avatarUrl="https://www.shutterstock.com/image-photo/passport-photo-portrait-woman-on-260nw-2438031869.jpg"
-        sessionDuration="45 min"
-        sessionTitle="Should you do GATE after Btech?"
-        sessionDescription="Join the session to know about tips and strategies to prepare for GATE Exam."
-        seatsBooked={9}
-        startDate="13/07/2024 08:30 PM"
-        backgroundImageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlygWcz51gyDexlstejSgZZ2LSxqF4rBz3wQ&s"
-      />
+      <div className="navbar-container">
+        <Navbar />
+      </div>
+
+      <div className="AppList">
+        <GateSessionCard
+          name="Ayush Adhikari"
+          handle="@Aerospace Engineer"
+          degree="M.Tech"
+          avatarUrl="https://www.shutterstock.com/image-photo/passport-photo-portrait-woman-on-260nw-2438031869.jpg"
+          sessionDuration="45 min"
+          sessionTitle="Should you do GATE after Btech?"
+          sessionDescription="Join the session to know about tips and strategies to prepare for GATE Exam."
+          seatsBooked={9}
+          startDate="13/07/2024 08:30 PM"
+          backgroundImageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlygWcz51gyDexlstejSgZZ2LSxqF4rBz3wQ&s"
+        />
+        <GateSessionCard
+          name="Ayush Adhikari"
+          handle="@Aerospace Engineer"
+          degree="M.Tech"
+          avatarUrl="https://www.shutterstock.com/image-photo/passport-photo-portrait-woman-on-260nw-2438031869.jpg"
+          sessionDuration="45 min"
+          sessionTitle="Should you do GATE after Btech?"
+          sessionDescription="Join the session to know about tips and strategies to prepare for GATE Exam."
+          seatsBooked={9}
+          startDate="13/07/2024 08:30 PM"
+          backgroundImageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlygWcz51gyDexlstejSgZZ2LSxqF4rBz3wQ&s"
+        />
+        <GateSessionCard
+          name="Ayush Adhikari"
+          handle="@Aerospace Engineer"
+          degree="M.Tech"
+          avatarUrl="https://www.shutterstock.com/image-photo/passport-photo-portrait-woman-on-260nw-2438031869.jpg"
+          sessionDuration="45 min"
+          sessionTitle="Should you do GATE after Btech?"
+          sessionDescription="Join the session to know about tips and strategies to prepare for GATE Exam."
+          seatsBooked={9}
+          startDate="13/07/2024 08:30 PM"
+          backgroundImageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlygWcz51gyDexlstejSgZZ2LSxqF4rBz3wQ&s"
+        />
+      </div>
     </div>
   );
 }
